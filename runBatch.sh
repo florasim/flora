06941a5484c8bc4369edcbb94528fbaa5315a4cb
Gopika Premsankar <gopika.premsankar@aalto.fi>
2018-03-29 15:16:53 +0300

    Update scripts to have correct path to executable

--- runBatch.sh
+++ runBatch.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 cd simulations
-opp_runall -j1 ../flora -u Cmdenv -c LoRaSim-NoADR -r 0 -n ../src:../simulations:../../inet/examples:../../inet/src:../../inet/tutorials -l ../../inet/src/INET batchTest.ini
+opp_runall -j1 ../src/flora -u Cmdenv -c LoRaSim-NoADR -r 0 -n ../src:../simulations:../../inet/examples:../../inet/src:../../inet/tutorials -l ../../inet/src/INET batchTest.ini
--- runSkeletons.sh
+++ runSkeletons.sh
@@ -5,5 +5,5 @@ FILES=networks/*
 for f in $FILES
 do
   echo "Started network $f:"
-  ../flora -u Cmdenv -n ../src:../simulations:../../inet/examples:../../inet/src:../../inet/tutorials -l ../../inet/src/INET -f loraNetwork_skeleton.ini -f $f
+  ../src/flora -u Cmdenv -n ../src:../simulations:../../inet/examples:../../inet/src:../../inet/tutorials -l ../../inet/src/INET -f loraNetwork_skeleton.ini -f $f
 done
