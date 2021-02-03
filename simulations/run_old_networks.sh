73871a1791975d3e0fa0ab76565b077491ca5f0e
Mariusz Slabicki <4083628+mariuszslabicki@users.noreply.github.com>
2021-02-03 11:13:57 +0100

    Add reference networks to test FLoRa

---
+++ simulations/run_old_networks.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+FILES=old_networks/*
+for f in $FILES
+do
+  echo "Started network $f:"
+  ../src/flora -u Cmdenv -n ../src:../simulations:../../inet4.3/examples:../../inet4.3/src:../../inet4.3/tutorials -l ../../inet4.3/src/INET -f $f &
+done
