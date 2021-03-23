9fc67d76624a267122931666596c65140cba3b23
Mariusz Słabicki <mariusz@Air-Mariusz.lan>
2021-03-23 21:37:31 +0100

    Cleanup

--- runBatch.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-cd simulations
-opp_runall -j1 ../src/flora -u Cmdenv -c LoRaSim-NoADR -r 0 -n ../src:../simulations:../../inet/examples:../../inet/src:../../inet/tutorials -l ../../inet/src/INET batchTest.ini
--- runSkeletons.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-cd simulations
-
-FILES=networks/*
-for f in $FILES
-do
-  echo "Started network $f:"
-  ../src/flora -u Cmdenv -n ../src:../simulations:../../inet/examples:../../inet/src:../../inet/tutorials -l ../../inet/src/INET -f loraNetwork_skeleton.ini -f $f
-done
