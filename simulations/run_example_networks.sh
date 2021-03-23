5f3ce663f69841714adab39763c1ccfd8cf9df2d
Mariusz Słabicki <mariusz@Air-Mariusz.lan>
2021-03-23 22:07:01 +0100

    Further cleanup

--- simulations/run_example_networks.sh
+++ simulations/run_example_networks.sh
@@ -4,5 +4,5 @@ FILES=examples/*
 for f in $FILES
 do
   echo "Started network $f:"
-  ../src/flora -u Cmdenv -n ../src:../simulations:../../inet4.3/examples:../../inet4.3/src:../../inet4.3/tutorials -l ../../inet4.3/src/INET -f $f &
+  ../src/flora -u Cmdenv -n ../src:../simulations:../../inet4.3/examples:../../inet4.3/src:../../inet4.3/tutorials -l ../../inet4.3/src/INET -f $f
 done
