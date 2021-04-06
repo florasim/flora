a0bbb2590e881c2eb33577e7f41b76d1adcf85fb
Rudolf Hornig <rudi@omnetpp.org>
2021-04-06 18:44:32 +0200

    added a hard error to LoRaGWMac to stop if INET is older than 4.3.1
    Technically this should go into some very common header file.

--- src/LoRa/LoRaGWMac.h
+++ src/LoRa/LoRaGWMac.h
@@ -16,6 +16,7 @@
 #ifndef LORA_LORAGWMAC_H_
 #define LORA_LORAGWMAC_H_
 
+#include "inet/common/INETDefs.h"
 #include "inet/physicallayer/wireless/common/contract/packetlevel/IRadio.h"
 #include "inet/linklayer/contract/IMacProtocol.h"
 #include "inet/linklayer/base/MacProtocolBase.h"
@@ -26,6 +27,9 @@
 #include "LoRaMacControlInfo_m.h"
 #include "LoRaMacFrame_m.h"
 
+#if INET_VERSION < 0x0403 || ( INET_VERSION == 0x0403 && INET_PATCH_LEVEL == 0x00 )
+#  error At least INET 4.3.1 is required. Please update your INET dependency and fully rebuild the project.
+#endif
 namespace flora {
 
 using namespace inet;
